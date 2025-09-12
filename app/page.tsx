2397dd67fe8bc062cbea2a32a41fda2154f0f53e
Dildora Muslimova <faithpacifica@yahoo.com>
2025-09-12 23:31:03 +0900

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,5 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  div className="flex min-h-screen flex-col items-center justify-between p-24">
-  );
+<div className="text-amber-400">Hello</div>  );
 }
