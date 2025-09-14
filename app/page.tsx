826854bd054a909ff24005afbe95ef187bda7d33
Dildora Muslimova <faithpacifica@yahoo.com>
2025-09-14 23:34:37 +0900

    add font

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="text-light-500 subtle-regular">
-      HELLO
-      <div className="w-[400px] h-[400px] primary-gradient"></div>
+    <div className="text-light-500 ">
+      <h1 className="font-inter">Hell0 1</h1>
+      <h1 className="font-space-grotesk">Hell0 2</h1>
     </div>
   );
 }
