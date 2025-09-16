e9e9a7f32554bf578c953cc518597fdcffb5d7ce
Dildora Muslimova <faithpacifica@yahoo.com>
2025-09-17 01:09:40 +0900

    create a theme toggler

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between bg:background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 gap-5">
+    <nav className="flex-between bg:background-light900_dark200  fixed z-50 w-full p-6 dark:shadow-none sm:px-12 gap-5">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
@@ -19,7 +20,9 @@ const Navbar = () => {
 
       <p>Global Search</p>
 
-      <div className="flex-between gap-5">Theme</div>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
     </nav>
   );
 };
