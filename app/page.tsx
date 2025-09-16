84cc4d1ecfe3be413fa6baa164414aef34923e92
Dildora Muslimova <faithpacifica@yahoo.com>
2025-09-17 00:17:46 +0900

    add Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-"use client"; // Mark as a client component for App Router
-import { useTheme } from "next-themes";
-
 export default function Home() {
-  const { theme, setTheme } = useTheme();
-  return (
-    <div className="text-purple-600">
-      <p>The current theme is: {theme}</p>
-      <button onClick={() => setTheme("light")}>Light Mode</button>
-      <br />
-      <button onClick={() => setTheme("dark")}>Dark Mode</button>
-    </div>
-  );
+  return <main className="text-blue-950">Main page</main>;
 }
