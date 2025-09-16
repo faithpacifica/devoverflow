3a323e2c2afb4f91f6d3726bcd49b3343c0cf518
Dildora Muslimova <faithpacifica@yahoo.com>
2025-09-16 21:39:25 +0900

    test light/dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+"use client"; // Mark as a client component for App Router
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
   return (
     <div className="text-purple-600">
-      <h1 className="font-inter">Hell0 1</h1>
-      <h1 className="font-space-grotesk">Hell0 2</h1>
+      <p>The current theme is: {theme}</p>
+      <button onClick={() => setTheme("light")}>Light Mode</button>
+      <br />
+      <button onClick={() => setTheme("dark")}>Dark Mode</button>
     </div>
   );
 }
