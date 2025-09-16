a2024530f50081bb0e7647fc8489907fe48ba0fe
Dildora Muslimova <faithpacifica@yahoo.com>
2025-09-16 21:35:02 +0900

    install next-themes and set themeProvider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="text-light-500 ">
+    <div className="text-purple-600">
       <h1 className="font-inter">Hell0 1</h1>
       <h1 className="font-space-grotesk">Hell0 2</h1>
     </div>
