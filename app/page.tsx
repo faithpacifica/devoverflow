7502ed22589e0948058123d227e8578b9ce0d210
Dildora Muslimova <faithpacifica@yahoo.com>
2025-09-17 22:34:16 +0900

    create route groups including auth route

--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <main className="text-blue-950">Main page</main>;
-}
--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -5,7 +5,7 @@ import Theme from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between bg:background-light900_dark200  fixed z-50 w-full p-6 dark:shadow-none sm:px-12 gap-5">
+    <nav className="flex-between bg:background-light900_dark200 dark:bg-dark-200  dark:dark-bg-light850_dark100 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 gap-5">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
@@ -14,7 +14,7 @@ const Navbar = () => {
           height={23}
         />
         <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dev<span className="text-primary-500">Flow</span>
+          Dev<span className="text-primary-500">Overflow</span>
         </p>
       </Link>
 
