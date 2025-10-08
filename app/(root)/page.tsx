ecf416924f7a901b4321441e7e387f84d0f442a5
Dildora Muslimova <faithpacifica@yahoo.com>
2025-10-08 21:39:30 +0900

    implement github signIn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <main className="text-9xl">Main page</main>;
-}
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from '@/constants/routes';
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next</h1>
+      <form className="px-10 pt-[100px]" action={async()=>{
+        "use server"
+        await signOut({redirectTo: ROUTES.SIGN_IN});
+      }}>
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
