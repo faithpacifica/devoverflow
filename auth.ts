f1ffa41b1079c0326633929b1719da228ce35e39
Dildora Muslimova <faithpacifica@yahoo.com>
2025-10-09 23:39:10 +0900

    implement google sign-in

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const runtime = "nodejs"; 
 
@@ -9,6 +10,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    Google
   ],
   secret: process.env.NEXTAUTH_SECRET,
 });
