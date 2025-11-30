df85fe6788f46bb23e6d876d9d8950588a4d60f2
Dildora Muslimova <faithpacifica@yahoo.com>
2025-12-01 01:16:43 +0900

    homepage update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,31 @@
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
-const Home = () => {
+const Home = async () => {
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next</h1>
-    
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION} className="max-sm:w-full">
+            Ask a Question
+          </Link>
+        </Button>
+      </section>
+      <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        LocalSearch
+      </section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 };
