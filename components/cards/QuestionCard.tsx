d95523f8b69be196878ed7711dbfb6f9da40aaac
Dildora Muslimova <faithpacifica@yahoo.com>
2026-01-02 02:10:50 +0900

    create reusable questionCard

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,7 +1,8 @@
 import ROUTES from "@/constants/routes";
 import { getTimeStamp } from "@/lib/utils";
 import Link from "next/link";
-
+import TagCard from "./TagCard";
+import Metric from "../Metric";
 
 interface Props {
   question: Question;
@@ -12,6 +13,7 @@ const QuestionCard = ({
 }: Props) => {
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div className="flex-1">
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
@@ -25,7 +27,54 @@ const QuestionCard = ({
         </div>
       </div>
 
-      
+      <div className="mt-3.5 flex w-full flex-wrap gap-2">
+        {tags.map((tag: Tag) => (
+          <TagCard key={tag._id} _id={tag._id} name={tag.name} compact />
+        ))}
+      </div>
+
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+
+        <Metric
+          imgUrl={author.image}
+          alt={author.name}
+          value={author.name}
+          title={`• asked ${getTimeStamp(createdAt)}`}
+          href={ROUTES.PROFILE(author._id)}
+          textStyles="body-medium text-dark400_light700"
+          isAuthor
+          imgStyles="w-[25px] h-[25px] object-cover"
+          titleStyles="max-sm:hidden"
+        />
+
+        <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
+
+          <Metric
+            imgUrl="/icons/like.svg"
+            alt="like"
+            value={upvotes}
+            title=" Votes"
+            textStyles="small-medium text-dark400_light800"
+          />
+
+          <Metric
+            imgUrl="/icons/message.svg"
+            alt="answers"
+            value={answers}
+            title=" Answers"
+            textStyles="small-medium text-dark400_light800"
+          />
+
+          <Metric
+            imgUrl="/icons/eye.svg"
+            alt="views"
+            value={views}
+            title=" Views"
+            textStyles="small-medium text-dark400_light800"
+          />
+
+        </div>
+      </div>
     </div>
   );
 };
