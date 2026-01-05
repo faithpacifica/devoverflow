a751dd5f0bb312aa491e1751d76564b4df89d8ba
Dildora Muslimova <faithpacifica@yahoo.com>
2026-01-05 21:51:17 +0900

    implement question model

---
+++ database/question.model.ts
@@ -0,0 +1,35 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+  title: string;
+  content: string;
+  tags: Types.ObjectId[];
+  views: number;
+  upvotes: number;
+  downvotes: number;
+  author: Types.ObjectId;
+  answers: number;
+}
+
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Question =
+  models?.Question || model<IQuestion>("Question", QuestionSchema);
+
+export default Question;
